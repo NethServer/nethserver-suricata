746f25c6be6a8b24aeb9e587a8fddfddf1ca4f6f
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2019-05-13 09:35:53 +0200

    cockpit. scaffolded ui,api and spec

---
+++ ui/src/views/Dashboard.vue
@@ -0,0 +1,51 @@
+<!--
+/*
+ * Copyright (C) 2019 Nethesis S.r.l.
+ * http://www.nethesis.it - nethserver@nethesis.it
+ *
+ * This script is part of NethServer.
+ *
+ * NethServer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * NethServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NethServer.  If not, see COPYING.
+ */
+-->
+
+<template>
+  <div>
+    <h2>{{ $t('dashboard.title') }}</h2>
+
+    <div v-if="!view.isLoaded" class="spinner spinner-lg view-spinner"></div>
+    <div v-if="view.isLoaded"></div>
+  </div>
+</template>
+
+<script>
+var nethserver = window.nethserver;
+var console = window.console;
+
+export default {
+  name: "Configuration",
+  mounted() {},
+  data() {
+    return {
+      view: {
+        isLoaded: false
+      }
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style>
+</style>
