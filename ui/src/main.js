fa529a1bb72bd601365675fb298f2602d4bf790f
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2020-07-03 15:53:27 +0200

    ui. added vue-good-table css

--- ui/src/main.js
+++ ui/src/main.js
@@ -34,6 +34,7 @@ import VueSuggestions from "v-suggestions";
 Vue.component("suggestions", VueSuggestions);
 
 import VueGoodTable from "vue-good-table";
+import 'vue-good-table/dist/vue-good-table.css'
 Vue.use(VueGoodTable);
 
 import DocInfo from "./components/DocInfo.vue";
